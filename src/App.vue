aee38f1a9fc3cf64b1e2774876d871d268aae29c
MariaJesús <jesup93@gmail.com>
2023-09-11 18:26:31 -0300

    ESTRUCTURA PAG CARACT

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="app">
+  <div>
     <NavbarRelbase/>
     <router-view></router-view>
     <FooterRelbase/>
--- src/views/caracteristicas/VentasRelbase.vue
+++ src/views/caracteristicas/VentasRelbase.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    
+    <div class="grey-bg-tittle">
+      <img src="../../assets/vectors/ventas.svg" alt="" class="caract-icono">
+      <h1>Ventas</h1>
+    </div>
+    <div class="container text-center main-sec">
+      <p class="subtittle-caract">
+        Con relBase gestiona todo tu proceso de venta.
+      </p>
+    </div>
   </div>
 </template>
+
+<script>
+
+
+export default {
+  name: 'VentasRelbase',
+  components: {
+  }
+}
+</script>
