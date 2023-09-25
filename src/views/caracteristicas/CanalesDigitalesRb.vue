874eab405c3e73d808ed2135e6b55d315a7b6ca7
MariaJesús <jesup93@gmail.com>
2023-09-25 16:03:10 -0300

    CANALES DIGITALES

--- src/views/caracteristicas/CanalesDigitalesRb.vue
+++ src/views/caracteristicas/CanalesDigitalesRb.vue
@@ -1,5 +1,44 @@
 <template>
   <div>
-    
+     <div class="grey-bg-tittle">
+      <img src="../../assets/vectors/canales-dig.svg" alt="" class="caract-icono">
+      <h1>Canales digitales</h1>
+    </div>
+    <div class="main-ventas container">
+      <div class="row">
+        <div class="col-xl-6 col-12 izq">
+          <h2>Vende en múltiples canales y mantén actualizado el stock en tiempo real.</h2>
+          <hr class="hr-caract">
+          <ul class="medium-p">
+            <li>
+              <span><img src="../../assets/vectors/check.svg" alt=""></span>
+              <p>Emisión automática de boleta y factura electrónica al generarse un nuevo pedido.</p>
+            </li>
+            <li>
+              <span><img src="../../assets/vectors/check.svg" alt=""></span>
+              <p>Sincronización de stock en tiempo real en todos los canales de venta conectados.</p>
+            </li>
+            <li>
+              <span><img src="../../assets/vectors/check.svg" alt=""></span>
+              <p>Sincronización de productos cada vez que existe un cambio en el canal de venta.</p>
+            </li>
+            <li>
+              <span><img src="../../assets/vectors/check.svg" alt=""></span>
+              <p>Disponible para: MercadoLibre, Ripley, WooCommerce, Prestashop, Jumpseller, Shopify , Centry, Paris y Falabella SellerCenter.</p>
+            </li>
+          </ul>
+        </div>
+        <div class="col-xl-6 col-12 pt-xl-0 pt-5 text-end">
+          <img src="../../assets/diagrama.png" alt="">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+
+export default {
+  name: 'CanalesDigitalesRb',
+}
+</script>
--- src/views/caracteristicas/ReportesRb.vue
+++ src/views/caracteristicas/ReportesRb.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    
+     <div class="grey-bg-tittle">
+      <img src="../../assets/vectors/reportes.svg" alt="" class="caract-icono">
+      <h1>Reportes</h1>
+    </div>
+    <div class="main-ventas container">
+      <div class="row">
+        <div class="col-xl-6 col-12 izq">
+          <h2>Módulo esencial para la toma de decisiones y gestión.</h2>
+          <hr class="hr-caract">
+          <p class="medium-p">Imprescindible para evaluar su desempeño en términos de ventas y para identificar patrones o tendencias en las ventas. Aquí se encuentra información sobre el volumen total de ventas, los productos más vendidos, los clientes más importantes, los márgenes de ganancia, las tendencias en las ventas, y cualquier otro dato relevante para evaluar el rendimiento de la empresa en términos de ventas.</p>
+        </div>
+        <div class="col-xl-6 col-12 pt-xl-0 pt-5 text-end">
+          <img src="../../assets/diagrama.png" alt="">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+
+export default {
+  name: 'ReportesRb',
+}
+</script>
