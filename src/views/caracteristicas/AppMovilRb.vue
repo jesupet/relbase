b36f7b5d1f15b6f42f7c5674895b2047300e58bc
MariaJesús <jesup93@gmail.com>
2023-09-25 16:31:35 -0300

    APP MOVIL

--- src/views/caracteristicas/AppMovilRb.vue
+++ src/views/caracteristicas/AppMovilRb.vue
@@ -1,5 +1,57 @@
 <template>
   <div>
-    
+     <div class="grey-bg-tittle">
+      <img src="../../assets/vectors/app-movil.svg" alt="" class="caract-icono">
+      <h1>App Móvil</h1>
+    </div>
+    <div class="main-ventas container">
+      <div class="row">
+        <div class="col-xl-6 col-12">
+          <h2>Descarga nuestra app y vende a través de nuestro punto de venta móvil.</h2>
+          <hr class="hr-caract">
+          <ul class="medium-p list">
+            <li class="el">
+              <span><img src="../../assets/vectors/check.svg" alt=""></span>
+              <p>Emisión de boleta y factura electrónica.</p>
+            </li>
+            <li class="el">
+              <span><img src="../../assets/vectors/check.svg" alt=""></span>
+              <p>Emisión de notas de ventas.</p>
+            </li>
+            <li class="el">
+              <span><img src="../../assets/vectors/check.svg" alt=""></span>
+              <p>Venta rápida sin detalle, ideal para aquellas ventas que no necesitas descontar stock.</p>
+            </li>
+            <li class="el">
+              <span><img src="../../assets/vectors/check.svg" alt=""></span>
+              <p>Venta con detalle, agrega tus productos y automáticamente se descontará el stock tanto en relBase como en tus canales digitales conectados.</p>
+            </li>
+            <li class="el">
+              <span><img src="../../assets/vectors/check.svg" alt=""></span>
+              <p>Compatible con impresoras bluetooth de 80mm o 58mm. Recomendamos las siguientes marcas:</p>
+            </li>
+            <ul class="mini-list">
+              <li>MobilePrinter</li>
+              <li>Goojprt PT 210</li>
+              <li>Xprinter XP-P101</li>
+            </ul>
+            <li class="el">
+              <span><img src="../../assets/vectors/check.svg" alt=""></span>
+              <p>App disponible en Google Play.</p>
+            </li>
+          </ul>
+        </div>
+        <div class="col-xl-6 col-12 pt-xl-0 pt-5 text-end">
+          <img src="../../assets/diagrama.png" alt="">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+
+export default {
+  name: 'AppMovilRb',
+}
+</script>
