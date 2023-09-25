60fc3345741f33977c4a432d27f9922fbcc6738a
MariaJesús <jesup93@gmail.com>
2023-09-25 15:41:02 -0300

    INVENTARIO, PTO VTA Y RECAUDACION

--- src/views/caracteristicas/PuntoVentaRb.vue
+++ src/views/caracteristicas/PuntoVentaRb.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    
+     <div class="grey-bg-tittle">
+      <img src="../../assets/vectors/pto-vta.svg" alt="" class="caract-icono">
+      <h1>Punto de venta</h1>
+    </div>
+    <div class="container main-sec">
+      <div class="main-ventas container">
+        <div class="row align-items-center">
+          <div class="col-xl-6 col-12 izq">
+            <p class="medium-p">Para llevar a cabo la transacción comercial entre el comprador y el vendedor fácilmente orientado a la tienda física.</p>
+          </div>
+          <div class="col-xl-6 col-12 pt-xl-0 pt-5 text-end">
+            <img src="../../assets/diagrama.png" alt="">
+          </div>
+        </div>
+      </div>  
+    </div>
   </div>
 </template>
+
+<script>
+
+export default {
+  name: 'PuntoVentaRb',
+}
+</script>
--- src/views/caracteristicas/RecaudacionRb.vue
+++ src/views/caracteristicas/RecaudacionRb.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    
+     <div class="grey-bg-tittle">
+      <img src="../../assets/vectors/recaudacion.svg" alt="" class="caract-icono">
+      <h1>Recaudación</h1>
+    </div>
+    <div class="container main-sec">
+      <div class="main-ventas container">
+        <div class="row align-items-center">
+          <div class="col-xl-6 col-12 izq">
+            <p class="medium-p">Revisión eficiente de todas las facturas en especial las pendientes o vencidas para hacer gestión de cobranza.</p>
+          </div>
+          <div class="col-xl-6 col-12 pt-xl-0 pt-5 text-end">
+            <img src="../../assets/diagrama.png" alt="">
+          </div>
+        </div>
+      </div>  
+    </div>
   </div>
 </template>
+
+<script>
+
+export default {
+  name: 'RecaudacionRb',
+}
+</script>
