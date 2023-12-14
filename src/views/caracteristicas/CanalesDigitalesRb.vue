737217447f24ca82d4037466affc9c4bd6ca7997
MariaJesús <jesup93@gmail.com>
2023-12-14 11:57:19 -0300

    AGREGADOS ICONOS CANALES DIGITALES

--- src/views/caracteristicas/CanalesDigitalesRb.vue
+++ src/views/caracteristicas/CanalesDigitalesRb.vue
@@ -4,7 +4,7 @@
       <img src="../../assets/vectors/canales-dig.svg" alt="" class="caract-icono">
       <h1>Canales digitales</h1>
     </div>
-    <div class="main-ventas container">
+    <div class="main-canales container">
       <div class="row">
         <div class="col-xl-6 col-12 izq order-xl-0 order-1">
           <h2>Vende en múltiples canales y mantén actualizado el stock en tiempo real.</h2>
@@ -28,8 +28,40 @@
             </li>
           </ul>
         </div>
-        <div class="col-xl-6 col-12 mockup-vtas order-xl-1 order-0">
-          <img src="../../assets/diagrama.png" alt="" class="img-fluid">
+        <div class="col-xl-6 col-12 canales-icons order-xl-1 order-0 row">
+          <div class="col-4">
+            <div class="logo-cont pt-4">
+              <img src="../../assets/logos/logos_bn/meli_bn.png" alt="">
+            </div>
+            <div class="logo-cont">
+              <img src="../../assets/logos/logos_bn/jumpseller_bn.png" alt="">
+            </div>
+            <div class="logo-cont">
+              <img src="../../assets/logos/logos_bn/prestashop_bn.png" alt="">
+            </div>
+          </div>
+          <div class="col-4 text-center">
+            <div class="logo-cont">
+              <img src="../../assets/logos/logos_bn/falabella_bn.png" alt="">
+            </div>
+            <div class="logo-cont">
+              <img src="../../assets/grey_logo.png" alt="" class="relbase-logo">
+            </div>
+            <div class="logo-cont pt-4 mt-4">
+              <img src="../../assets/logos/logos_bn/wc_bn.png" alt="">
+            </div>
+          </div>
+          <div class="col-4 text-end">
+            <div class="logo-cont mt-4">
+              <img src="../../assets/logos/logos_bn/ripley_bn.webp" alt="">
+            </div>
+            <div class="logo-cont">
+              <img src="../../assets/logos/logos_bn/paris_bn.png" alt="">
+            </div>
+            <div class="logo-cont">
+              <img src="../../assets/logos/logos_bn/shopify_bn.png" alt="">
+            </div>
+          </div>
         </div>
       </div>
     </div>
